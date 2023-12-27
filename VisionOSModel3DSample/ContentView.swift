e5f003a586670a5c583484cf3630ea2c8fa59f2c
tokufxug <tokufxug@gmail.com>
2023-12-27 14:35:42 +0900

    visionos model3d sample

--- VisionOSModel3DSample/ContentView.swift
+++ VisionOSModel3DSample/ContentView.swift
@@ -7,15 +7,16 @@
 
 import SwiftUI
 import RealityKit
-import RealityKitContent
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
-
-            Text("Hello, world!")
+            Text("Model3D").font(.extraLargeTitle2)
+            Model3D(named:"teapot") {model in
+                model
+            } placeholder: {
+                ProgressView()
+            }
         }
         .padding()
     }
