58d856d77dd93cf6265f938e34c3f4dcadc63453
tokufxug <tokufxug@gmail.com>
2023-12-27 14:01:56 +0900

    Initial Commit

---
+++ VisionOSModel3DSample/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  VisionOSModel3DSample
+//
+//  Created by Sadao Tokuyama on 12/27/23.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ VisionOSModel3DSample/VisionOSModel3DSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOSModel3DSampleApp.swift
+//  VisionOSModel3DSample
+//
+//  Created by Sadao Tokuyama on 12/27/23.
+//
+
+import SwiftUI
+
+@main
+struct VisionOSModel3DSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
